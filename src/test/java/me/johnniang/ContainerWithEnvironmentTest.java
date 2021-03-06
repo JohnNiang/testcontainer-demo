889a165c5ec7a083dab561b42ccf8154362df902
johnniang <johnniang@foxmail.com>
2021-03-06 17:47:10 +0800

    Refine ContainerWithEnvironmentTest

--- src/test/java/me/johnniang/ContainerWithEnvironmentTest.java
+++ src/test/java/me/johnniang/ContainerWithEnvironmentTest.java
@@ -3,6 +3,7 @@ package me.johnniang;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -21,7 +23,7 @@ import org.testcontainers.utility.DockerImageName;
  * @author johnniang
  */
 @Testcontainers
-class ContainerWithEnvironment {
+class ContainerWithEnvironmentTest {
 
 	Logger log = LoggerFactory.getLogger(getClass());
 
@@ -29,26 +31,26 @@ class ContainerWithEnvironment {
 
 	@Container
 	GenericContainer<?> alpine = new GenericContainer<>(alpineImage)
-			.withExposedPorts(18888)
+			.withExposedPorts(80)
 			.withEnv("HELLO", "WORLD")
-			.withCommand("/bin/sh", "-c", "while true; do echo -n \"$HELLO\" | nc -l -p 18888; done")
-			.withLogConsumer(outputFrame -> {
-				log.debug("Container output ===|{}", outputFrame.getUtf8String());
-			});
+			.withCommand("/bin/sh", "-c", "while true; do echo -n \"$HELLO\" | nc -l -p 80; done")
+			.withLogConsumer(new Slf4jLogConsumer(this.log));
 
 	@Test
 	void fetchEnvironmentWithSocket() throws IOException {
-		int port = alpine.getFirstMappedPort();
+		int port = this.alpine.getFirstMappedPort();
 
 		try (Socket socket = new Socket("localhost", port)) {
-			try (InputStream is = socket.getInputStream()) {
-				try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+			try (OutputStream os = socket.getOutputStream();
+				 InputStream is = socket.getInputStream()) {
+				os.write("Hello\n".getBytes(StandardCharsets.UTF_8));
+				try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 					byte[] buffer = new byte[1024];
 					int read;
 					while ((read = is.read(buffer)) != -1) {
-						os.write(buffer, 0, read);
+						baos.write(buffer, 0, read);
 					}
-					String response = os.toString(StandardCharsets.UTF_8);
+					String response = baos.toString(StandardCharsets.UTF_8);
 					Assertions.assertEquals("WORLD", response);
 				}
 			}
